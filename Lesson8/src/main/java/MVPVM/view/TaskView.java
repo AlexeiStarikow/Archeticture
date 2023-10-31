5bbd6a9f76841f4cc14f3a8add28ec3a631925de
AlexeiStarikow <alexeistarikow@yandex.ru>
2023-10-31 12:35:31 +0300

    add Lesson8, HW8

---
+++ Lesson8/src/main/java/MVPVM/view/TaskView.java
@@ -0,0 +1,9 @@
+package main.java.MVPVM.view;
+
+import MVPVM.viewModel.TaskViewModel;
+
+import java.util.List;
+
+public interface TaskView {
+    void showTasks(List<TaskViewModel> taskViewModels);
+}
---
+++ Lesson8/src/main/java/mvcExample/controller/PrinterController.java
@@ -0,0 +1,16 @@
+package main.java.mvcExample.controller;
+
+import mvcExample.model.PrinterUpperCase;
+
+public class PrinterController {
+    PrinterUpperCase printerUpperCase;
+
+    public PrinterController(PrinterUpperCase printerUpperCase) {
+        this.printerUpperCase = printerUpperCase;
+    }
+
+    public void print(String str) {
+        printerUpperCase.print(str);
+    }
+
+}
