d96c043d7ad9e19c7e9ea643323e55e494c50877
AlexeiStarikow <alexeistarikow@yandex.ru>
2023-11-07 15:44:28 +0300

    add Lesson10

---
+++ Lesson10/src/main/java/slp/Product.java
@@ -0,0 +1,23 @@
+package main.java.slp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Пример сущности
+public class Product {
+    private int id;
+    private String name;
+
+    public Product(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
